c15a04a30f2f48a3c557f059a7929eec148c7488
bavegar2001 <32004404-bavegar2001@users.noreply.replit.com>
2024-02-28 15:23:26 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import 'src/navegador.scss'
+
+export default function App() {
+  return (
+    <main>
+      React ⚛️ + Vite ⚡ + Replit
+      <nav >
+      </nav>
+    </main>
+  )
+}
